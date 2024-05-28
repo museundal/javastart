a0b9050d0f1064fc882c35de4e119e94bada5deb
museundal <ehdus1105@gmail.com>
2024-05-29 00:49:01 +0900

    주석

---
+++ src/CommentJava.java
@@ -0,0 +1,14 @@
+package src;
+
+public class CommentJava {
+    public static void main(String[] args){
+        System.out.println("Hello World"); // 헬로 월드를 출력합니다.
+        //System.out.println("Hello World2");
+        /* 여러줄 주석
+        System.out.println("Hello World3");
+        System.out.println("Hello World4");
+        */
+
+
+    }
+}
